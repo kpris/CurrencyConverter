080b8a49ad74ba73a62508e09174af08842e60e2
Priscilla Bevis <priscillabevis@Priscillas-MacBook-Air.local>
2015-10-11 09:42:34 +1100

    initial commit

--- CurrencyConverter/HomeViewController.swift
+++ CurrencyConverter/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  CurrencyConverter
 //
 //  Created by Priscilla Bevis on 11/10/2015.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
