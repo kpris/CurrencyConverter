53e3128e8cab759650fac24aff36c4c0b5cdc6fa
Priscilla Bevis <priscillabevis@priscillas-macbook-air.local>
2015-10-11 09:38:34 +1100

    Initial Commit

---
+++ CurrencyConverterTests/CurrencyConverterTests.swift
@@ -0,0 +1,36 @@
+//
+//  CurrencyConverterTests.swift
+//  CurrencyConverterTests
+//
+//  Created by Priscilla Bevis on 11/10/2015.
+//  Copyright © 2015 Priscilla. All rights reserved.
+//
+
+import XCTest
+@testable import CurrencyConverter
+
+class CurrencyConverterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
