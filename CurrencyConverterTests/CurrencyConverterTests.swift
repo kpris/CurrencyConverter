f92f212e3a0009c1bde1960411b5682985ff5d08
PriscillaBevis <priscillabevis@Priscillas-MacBook-Air.local>
2015-10-11 10:23:45 +1100

    adding initial tests

--- CurrencyConverterTests/CurrencyConverterTests.swift
+++ CurrencyConverterTests/CurrencyConverterTests.swift
@@ -11,26 +11,20 @@ import XCTest
 
 class CurrencyConverterTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testThatResultsExistWhenValueAdded() {
+        let homeVC = UIStoryboard(name: "HomeVC", bundle: nil).instantiateInitialViewController() as! HomeViewController
+        homeVC.loadView()
+        
+        homeVC.entryField.text = "100"
+        XCTAssertTrue(homeVC.resultLabel.text?.characters.count > 0)
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    func testThatResultIsEmptyWhenValueEmpty() {
+        let homeVC = UIStoryboard(name: "HomeVC", bundle: nil).instantiateInitialViewController() as! HomeViewController
+        homeVC.loadView()
+        
+        homeVC.entryField.text = ""
+        XCTAssertTrue(homeVC.resultLabel.text?.characters.count == 0)
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+
 }
